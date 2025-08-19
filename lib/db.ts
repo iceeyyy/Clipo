985ec947d66a0e9d9a8fdd590ab3eabfb3bdb53d
Achal Sharma <sharmaachal02@gmail.com>
2025-08-19 17:28:51 +0530

    first commit

---
+++ lib/db.ts
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+
+const MONGODB_URI = process.env.MONGODB_URL!;
+
+if(!MONGODB_URI) {
+  throw new Error("Please define the MONGODB_URL environment variable inside .env.local");
+}
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+export async function dbConnect() {
+
+    if(cached.conn) { //if connection already exists
+      return cached.conn;
+    }
+
+    if(!cached.promise) {
+        //if promise does not exist, create a new one
+        const opts = {
+          bufferCommands: true,
+          maxPoolSize: 10
+        }
+        cached.promise = mongoose.connect(MONGODB_URI,opts).then(()=> mongoose.connection);
+   }
+
+   try{
+      cached.conn = await cached.promise;  // Wait for the promise to resolve
+   }
+
+   catch (e) {
+      cached.promise = null;  //incase of error, reset the promise
+      throw e;
+   }
+   
+   return cached.conn;
+}
---
+++ src/app/api/auth/register/route.ts
@@ -0,0 +1,44 @@
+import { NextResponse,NextRequest } from "next/server"
+import User from "../../../../../models/User";
+import { dbConnect } from "../../../../../lib/db";
+
+export async function POST (request: NextRequest) {
+    try{
+        const {email, password} = await request.json();
+
+        if(!email || !password) {
+            return NextResponse.json(
+                {message:"Email and password are required"}, 
+                {status:400}
+            );
+        }
+
+        await dbConnect(); // Ensure the database connection is established
+
+        const existingUser = await User.findOne({email}); 
+        if(existingUser){
+            return NextResponse.json(
+                {message:"Email already exists"}, 
+                {status:400}
+            );
+        }
+
+        await User.create({
+            email,
+            password
+        })
+
+        return NextResponse.json(
+            {message:"User Created Successfully"},
+            {status: 201}
+        );
+    }
+    catch (error) {
+        console.error(error);
+        return NextResponse.json(
+            {message:"Failed to Create User"},
+            {status: 500}
+        );
+    }
+}
+
